d50fe89e602ea3f5a4d99eb517cdf5e312992be4
deka4core <borodaenglish@gmail.com>
2022-02-05 17:02:13 +0300

    -You can move with up/down/left/right arrows

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import pygame
 import requests
 
 map_request = "http://static-maps.yandex.ru/1.x/"
-ll = input("Введите координаты в формате (x,y): ")
+ll = list(map(float, input("Введите координаты в формате (x,y): ").split(',')))
 spn = float(input("Введите коэффициент масштабирования: "))
 map_file = "map.png"
 
 
-def do_map(spn, map_file):
-    params = {"ll": ll,
+def do_map(spn, ll, map_file):
+    params = {"ll": ','.join(list(map(str, ll))),
               "spn": f"{spn},{spn}",
               "l": "map"}
     response = requests.get(map_request, params=params)
@@ -25,7 +25,7 @@ def do_map(spn, map_file):
         file.write(response.content)
 
 
-do_map(spn, map_file)
+do_map(spn, ll, map_file)
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
@@ -39,10 +39,18 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
                 spn = spn * 2 if spn * 2 <= 50 else spn
-                do_map(spn, map_file)
             elif event.key == pygame.K_PAGEDOWN:
                 spn = spn / 2 if spn / 2 >= 0.002 else spn
-                do_map(spn, map_file)
+            elif event.key == pygame.K_UP:
+                ll[1] = ll[1] + spn * 2 if ll[1] + spn * 2 < 85 else ll[1]
+            elif event.key == pygame.K_DOWN:
+                ll[1] = ll[1] - spn * 2 if ll[1] - spn * 2 > -85 else ll[1]
+            elif event.key == pygame.K_LEFT:
+                ll[0] = ll[0] - spn * 2 if ll[0] - spn * 2 > -180 else ll[0]
+            elif event.key == pygame.K_RIGHT:
+                ll[0] = ll[0] + spn * 2 if ll[0] + spn * 2 < 180 else ll[0]
+            do_map(spn, ll, map_file)
+
     # Рисуем картинку, загружаемую из только что созданного файла.
     screen.blit(pygame.image.load(map_file), (0, 0))
     # Переключаем экран, и ждем закрытия окна.
